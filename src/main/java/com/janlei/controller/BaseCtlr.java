40c3bc946e38ec1589645c7b93a9e3a9f70a3ca3
Janlei-CN <janlei4597@gmail.com>
2020-03-27 23:47:58 +0800

    update and use yaml config

--- src/main/java/com/janlei/controller/BaseCtlr.java
+++ src/main/java/com/janlei/controller/BaseCtlr.java
@@ -1,7 +1,9 @@
 package com.janlei.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
@@ -12,9 +14,27 @@ public class BaseCtlr {
     @Autowired
     private DataSource dataSource;
 
-    @GetMapping         //@RequestMapping(method = {RequestMethod.GET})
+    @Value("${itcast.url}")
+    private String url1;
+
+    @Value("${bili.url}")
+    private String url2;
+
+    @GetMapping(value = "hi")         //@RequestMapping(method = {RequestMethod.GET})
     public String hi(){
+
+        System.out.println(url1 + url2);
         System.out.println(dataSource);
-        return "hello world!";
+        return "hi world!";
     }
+
+
+    @PostMapping(value = "hello")
+    public String hello(){
+
+        System.out.println(url1 + url2);
+        System.out.println(dataSource);
+        return "hi world!";
+    }
+
 }
